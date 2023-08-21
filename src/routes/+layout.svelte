8a2599d0ea27151c1c6bd46475089ec184ef7323
David Kirby <david@kinetic.com>
2023-08-21 10:19:58 -0500

    added images

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "$lib/app.css";
+</script>
+
+<div class="container-fluid d-flex flex-column min-vh-100 m-0 p-0">
+  <header>
+    <h1>KWorld game guides</h1>
+  </header>
+
+  <nav />
+
+  <main class="flex-grow-1">
+    <slot />
+  </main>
+
+  <footer>
+    <p>david kirby</p>
+  </footer>
+</div>
