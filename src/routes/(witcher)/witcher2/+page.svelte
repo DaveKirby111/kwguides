da964b39b64daff006bfff5b1957fbd1f19f0995
DaveKirby111 <112968620+DaveKirby111@users.noreply.github.com>
2025-03-27 06:06:29 -0500

    added to witcher 2

--- src/routes/(witcher)/witcher2/+page.svelte
+++ src/routes/(witcher)/witcher2/+page.svelte
@@ -17,12 +17,47 @@
   <div class="guide-sections">
     <p class="fw-bold text-decoration-underline">Sections</p>
     <ul class="skip">
-      <li><a href="#tombs">tombs</a></li>
+      <li><a href="#tombs">Side Quests</a></li>
     </ul>
+  </div>
+
+  <div id="side">
+    <h3>side quests</h3>
+  <div id="sideq">
+    <p class="fw-bold text-decoration-underline">Little Sisters</p>
+    <p>Order:</p>
+    <ul class="sisters">
+      <li>v</li>
+      <li>diamond</li>
+      <li>upside down v</li>
+    </ul>
+  </div>
+
+  <div id="sideq">
+    <p class="fw-bold text-decoration-underline">Gargoyle Contract</p>
+    <p>Rune Order:</p>
+    <ul class="runes">
+      <li>left of the entrance</li>
+      <li>right wall</li>
+      <li>on the floor to the left</li>
+      <li>on the floor in front of treasure chest</li>
+    </ul>
+  </div>
   </div>
 
   <a href="#top" class="skiplink">Top</a>
 </div>
 
 <style>
+  h3{
+    text-transform: capitalize;
+  }
+
+  #sideq{
+    margin-bottom: 30px;
+  }
+
+.sisters {
+text-transform: capitalize;
+}
 </style>
